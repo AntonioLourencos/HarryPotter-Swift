7c05f7e4a50c37e1dbba89855c657507be90eadc
Antonio Lourencos <antoniolourencosdeveloper@gmail.com>
2023-04-08 18:48:37 -0300

    Initial Commit

---
+++ HarryPotter/HarryPotterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarryPotterApp.swift
+//  HarryPotter
+//
+//  Created by Antonio Lourencos on 08/04/23.
+//
+
+import SwiftUI
+
+@main
+struct HarryPotterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
